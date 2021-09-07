8c3fdf771ff4223788d8cc495f1b3cb66d71752d
Kyle-Wi <black1848@naver.com>
2021-09-07 16:59:09 +0900

    20210907 "nginx, profile updated"

--- src/test/java/com/orangeblue/springbootaws/web/ProfileControllerTest.java
+++ src/test/java/com/orangeblue/springbootaws/web/ProfileControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.env.MockEnvironment;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -26,7 +25,7 @@ public class ProfileControllerTest {
     @Test
     public void profile은_인증없이_호출된다() throws Exception {
         
-        String expected = "default";
+        String expected = "real";
 
         ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
         
@@ -35,70 +34,6 @@ public class ProfileControllerTest {
         assertThat(response.getBody()).isEqualTo(expected);
     }
 
-
-    @Test
-    public void real_profile이_조회된다() {
-        //given
-
-        String expectedProfile = "real";
-        MockEnvironment env = new MockEnvironment();
-
-        env.addActiveProfile(expectedProfile);
-        env.addActiveProfile("oauth");
-        env.addActiveProfile("real-db");
-
-        ProfileController controller = new ProfileController(env);
-
-        // when
-
-        String profile = controller.profile();
-
-        //then
-
-        assertThat(profile).isEqualTo(expectedProfile);
-
-    }
-
-    @Test
-    public void real_profile이_없으면_첫_번째가_조회된다() {
-        //given
-
-        String expectedProfile = "oauth";
-        MockEnvironment env = new MockEnvironment();
-
-        env.addActiveProfile(expectedProfile);
-        env.addActiveProfile("read-db");
-
-        ProfileController controller = new ProfileController(env);
-
-        //when
-
-        String profile = controller.profile();
-
-        //then
-
-        assertThat(profile).isEqualTo(expectedProfile);
-
-    }
-    
-
-    @Test
-    public void active_profile이_없으면_default가_조회된다(){
-
-        //given
-
-        String expectedProfile = "default";
-        MockEnvironment env = new MockEnvironment();
-
-        ProfileController controller = new ProfileController(env);
-
-        // when
-        String profile = controller.profile();
-
-        // then
-
-        assertThat(profile).isEqualTo(expectedProfile);
-    }
 
 }
 
