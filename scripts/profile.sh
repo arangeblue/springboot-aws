047956348c03674e0671f42cbed820f963bb24e5
Kyle-Wi <black1848@naver.com>
2021-09-07 20:26:21 +0900

    20210907 ".sh file updated"

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -4,7 +4,7 @@ function find_idle_profile()
 {
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-    if [ "${RESPONSE_CODE}" -ge 400 ]
+    if [ "${RESPONSE_CODE}" -ge 400 ];
     then
         CURRENT_PROFILE=real2
     else
@@ -12,7 +12,7 @@ function find_idle_profile()
     fi
 
 
-    if [ "${CURRENT_PROFILE}" == real1 ]
+    if [ "${CURRENT_PROFILE}" == real1 ];
     then
         IDLE_PROFILE=real2
     else
@@ -27,7 +27,7 @@ function find_idle_port()
 {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [ ${IDLE_PROFILE} == real1 ]
+    if [ "${IDLE_PROFILE}" == real1 ];
     then
         echo "8081"
     else
